9153feada537ce8f26a4bd7eccee9f470afdb18e
malatto <moon2263@naver.com>
2019-02-12 23:17:18 -0800

    [modify] bcon.py

--- bcon.py
+++ bcon.py
@@ -1,6 +1,5 @@
 from multiprocessing import Pool
 from scapy.all import *
-import time
 
 def mySendp(x):
 	ssidList = ['Frozen','Let_it_go','do_you_wanna_be_a_snow_man?','love_is_open_door','LionKing','scar','simba']
@@ -8,10 +7,8 @@ def mySendp(x):
 	sMac = RandMAC()
 	dot11 = Dot11(type=0,subtype=0x08,addr1=rMac,addr2=sMac,addr3=sMac)
 	bcon = Dot11Beacon()
-	print "num : %d" % x
 	essid = Dot11Elt(ID='SSID',info=ssidList[x],len=len(ssidList[x]))
 	frame = RadioTap()/dot11/bcon/essid
-	start_time = time.time()
 	sendp(frame,iface='mon0',inter=0.100,loop=1)
 
 pool = Pool(7)
